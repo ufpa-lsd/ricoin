d66d7a1cef528dec0ebd0e74b2cd9a0cdaee99df
lubien <lubien1996@gmail.com>
2019-10-12 17:48:31 -0300

    Setup basic lib and echo bot

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+const request = require('./request')
 
-var net = require('net');
+const timeout = time => new Promise(resolve =>
+    setTimeout(resolve, time)
+)
 
-var server = net.createServer(function(socket) {
-	socket.write('Echo server\r\n');
-	socket.pipe(socket);
-});
+async function main () {
+    let offset = 0
 
-server.listen(1337, '127.0.0.1');
+    while (true) {
+
+        const { result} = await request('getUpdates', {
+            offset
+        })
+
+
+        for (let update of result) {
+            prettyPrint(update)
+            const {message} = update
+            
+            if (message && !message.from.is_bot) {
+                await request('sendMessage', {
+                    chat_id: update.message.chat.id,
+                    text: `Did you just say '${message.text}'?`
+                })
+                    .then(console.log)
+                    .catch(console.error)
+            }
+
+            // Update offset
+            offset = update.update_id + 1
+        }
+        
+        // console.log('Timeout')
+        await timeout(500)
+    }
+}
+
+main()
+
+function prettyPrint(data) {
+    console.log(JSON.stringify(data, undefined, 2))
+}
